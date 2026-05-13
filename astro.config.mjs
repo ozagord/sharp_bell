1bdcdafed6458f33e5c2bbe94da14ced13dc05e3
Osvaldo Zagordi <osvaldo.zagordi+git@gmail.com>
2026-05-13 09:27:04 +0200

    Support for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
-  },
+  }
 });
